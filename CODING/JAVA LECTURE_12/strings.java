a48694979bce49ac1472f9bad16d8127c25fa2cf
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-07 08:31:16 +0500

    commit

---
+++ CODING/JAVA LECTURE_12/strings.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class strings {
+    public static void main(String args[]){
+        
+        // String name = "MAS";
+        // String fullName = "MASAASMJ";
+        // String sentence = "My name is TS";
+        
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("Your name is : "+ name);
+        
+        
+        //Concatination
+        // String firstName = "AAS";
+        // String lastName = "MAS";
+        // String fullName = firstName + "@" + lastName;
+        // System.out.println(fullName.length());
+        
+        // for(int i = 0; i<fullName.length(); i++){
+        //     System.out.println(fullName.charAt(i));
+        // }
+        
+        
+        //Compare
+        String name1 = "AAS";
+        String name2 = "AAS";
+        
+        //1st > 2nd +ve value
+        //1st == 2nd : 0
+        //1st < 2nd -ve value
+        // if(name1.compareTo(name2) == 0) {
+        //     System.out.println("Strings are equal");
+        // } else {
+        //     System.out.println("Strings are not equal");
+        // }
+        
+        
+        
+        // if(name1 == 
+        // name2) {
+        //     System.out.println("Strings are equal");
+        // } else {
+        //     System.out.println("Strings are not equal");
+        // }
+        
+        // if(new String("Tony") == new String("Tony")){
+        //     System.out.println("Strings are equal");} else {
+        //         System.out.println("Strings are not equal");
+        //     }
+        
+        String sentence = "My name is MAS";
+        // substring(beg ind, end ind);
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
+        }
+    }
+
+
+
+
+
+
+

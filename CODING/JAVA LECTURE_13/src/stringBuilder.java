8ccfc3041bbc6e862778700dfbb0fa8d369e50df
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-08 00:15:23 +0500

    commit

---
+++ CODING/JAVA LECTURE_13/src/stringBuilder.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class stringBuilder{
+    public static void main(String args[]){
+        // StringBuilder sb = new StringBuilder("MAS");
+        // // System.out.println(sb);
+        
+        // //char at index 0 
+        // // System.out.println(sb.charAt(0));
+        
+        // //set char at index
+        // // sb.setcharAt(0,'A');
+        // // System.out.println(sb);
+        
+        // //set char at index
+        // // sb.insert(2,'r');
+        // // System.out.println(sb);
+        
+        // // //delete char at index
+        // // sb.delete(2,3);//start end value
+        // // System.out.println(sb);
+        
+        // // sb.append("AAS");
+        // // sb.append("MJ");
+        // // System.out.println(sb);
+        // // System.out.println(sb.length());
+        
+        // for(int i = 0; i<sb.length()/2;i++){
+        //     int front = i;
+        //     int back = sb.length() -1 - i;
+            
+        //     char frontChar = sb.charAt(front);
+        //     char backChar = sb.charAt(back);
+            
+        //     sb.setCharAt(front, backChar);
+        //     sb.setCharAt(back, frontChar);
+        // }
+        // System.out.println(sb);
+
+
+        System.out.println("MAS");
+    }
+}

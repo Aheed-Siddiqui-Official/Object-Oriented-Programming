f2bf5755baeeba3165fe1fc7bc643629d8870594
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-10 20:02:13 +0500

    commit

---
+++ CODING/JAVA LECTURE_2/main.java
@@ -0,0 +1,55 @@
+//*****Single Word******/
+// import java.util.*;
+// public class Main {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String name = sc.next();
+//     System.out.println(name);
+//     }
+
+// //*****Single Word******/
+// import java.util.*;
+// public class Main {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String name = sc.nextLine();
+//     System.out.println(name);
+//     }
+// }
+
+// import java.util.*;
+// public class Main {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String name = sc.next();
+//     System.out.println(name);
+//     }
+// }
+
+
+// import java.util.*;
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         float a = sc.nextFloat();
+//         float b = sc.nextFloat();
+//         float sum = a+b;
+//         System.out.println(sum);
+//     }
+// }
+
+// import java.util.*;
+// public class main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter value for a: ");
+//         float a = sc.nextFloat();
+//         System.out.print("Enter value for b: ");
+//         float b = sc.nextFloat();
+//         System.out.print("Enter value for c: ");
+//         float c = sc.nextFloat();
+//         float solve = ((a+b+c*b-c/a)/(a+b+c));
+//         System.out.print("Solution: ");
+//         System.out.println(solve);
+//     }
+// }

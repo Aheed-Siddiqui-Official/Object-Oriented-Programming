b26b36619043846a3023885fb808ba214c0783fa
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-11 07:54:00 +0500

    commit

---
+++ CODING/JAVA LECTURE_24/src/BANK.java
@@ -0,0 +1,58 @@
+
+
+//Access Modifiers
+
+//Public
+// class Account {
+//     public String name;
+// }
+
+//Default
+// class Account {
+//      String name;
+// }
+
+//Protected
+// class Account {
+//     protected String name;
+// }
+
+//Private
+// class Account {
+//     private String name;
+// }
+
+
+// class Account {
+//     public String name;
+//     protected String email;
+//     private String password;
+    
+//     //getters & setters
+//     public String getPassword() {
+//         return this.password;
+//     }
+
+//     public void setPassword(String pass) {
+//         this.password = pass;
+//     }
+// }
+
+// public class BANK {
+//     public static void main(String[] args) {
+//         Account account1 = new Account();
+//         account1.name = "MAS";
+//         account1.email = "MAS";
+//         account1.setPassword("pass");
+//         System.out.println(account1.getPassword());
+//     }
+    
+// }
+
+
+// public class BANK {
+//     public static void main(String[] args) {
+//         System.out.println("MAS");
+//     }
+    
+// }

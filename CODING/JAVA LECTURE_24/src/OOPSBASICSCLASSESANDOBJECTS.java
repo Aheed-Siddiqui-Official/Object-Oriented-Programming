5aadd7116960c2de3f20bff9abd959e5833faa33
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-12 08:35:29 +0500

    commit

---
+++ CODING/JAVA LECTURE_24/src/OOPSBASICSCLASSESANDOBJECTS.java
@@ -0,0 +1,77 @@
+// //******** BASICS OF OBJECTS AND CLASSES *********
+
+// class Pen{
+//     //     String color;
+//     //     String type;//gel,ball
+        
+//     //     public void write() {
+//     //         System.out.println("Writing Something");
+//     //     }
+        
+//     //     public void printColor() {
+//     //         System.out.println(this.color);
+//     //     }
+//     }
+    
+//     class Student{
+//         // String name;
+//         // int age;
+        
+//         // public void printInfo() {
+//         //     System.out.println(this.name);
+//         //     System.out.println(this.age);
+//         // }
+        
+//         //NON PARAMETERIZED CONSTRUCTOR
+//         // Student(){
+//         //     System.out.println("constructor called");
+//         // }
+        
+//         //PARAMETERIZED CONSTRUCTOR
+//         // Student(String name, int age) {
+//         //     this.name = name;
+//         //     this.age = age;
+//         // }
+        
+//         //COPY CONSTRUCTORS
+//         // Student(Student s2) {
+//         //     this.name = s2.name;
+//         //     this.age = s2.age;
+//         // }
+        
+//         // Student() {
+            
+//         // }
+//     }
+    
+    
+    
+    
+//     public class OOPSBASICSCLASSESANDOBJECTS {
+//         public static void main(String[] args) {
+//             // Pen pen1 = new Pen();
+//             // pen1.color = "red";
+//             // pen1.type = "gel";
+            
+//             // Pen pen2 = new Pen();
+//             // pen2.color = "black";
+//             // pen2.type = "ballpoint";
+            
+//             // pen1.printColor();
+//             // pen2.printColor();
+            
+//             // Student s1 = new Student();
+//             // s1.name = "MAS";
+//             // s1.age = 21;
+            
+//             // Student s2 = new Student(s1);
+//             // s2.printInfo();
+//         }
+//     }
+
+public class OOPSBASICSCLASSESANDOBJECTS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

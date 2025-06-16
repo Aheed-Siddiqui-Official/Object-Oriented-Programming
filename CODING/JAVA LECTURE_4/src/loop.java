4e3fa6cae9d0cd5f8e29d24fa222ae7d94c36273
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-16 08:22:15 +0500

    commit

---
+++ CODING/JAVA LECTURE_4/src/loop.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class loop {
+    public static void main(String[] args) {
+
+                    //  **********************FOR LOOP************************
+        // for(int a = 1 ; a <=100 ; a = a + 1){
+        //     System.out.println("Hello World");
+        // }
+
+        // for(int a = 0 ; a <=10 ; a++){
+        //     System.out.println(a);
+        // }
+
+
+                     //  **********************WHILE LOOP************************
+        
+        // int i = 0;
+        // while(i < 12){
+        // System.out.println(i);
+        // i = i + 1; // i++
+        // }         
+
+
+                    //  **********************DO WHILE LOOP************************
+            
+        // int i = 0;
+        // do {
+        // System.out.println(i);
+        // i++;
+        // } while(i < 11);
+
+        
+        
+        
+        
+        
+                    //  **********************PRACTICE************************
+        
+        // int i = 12;
+        // while(i < 11){
+        //     System.out.println("MAS");
+        // }
+
+
+
+        // int i = 12;
+        // do{
+        //     System.out.println("MAS");
+        //     i++;
+        // } while(i < 11);
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number : ");
+        // int num = sc.nextInt();
+        // int sum = 0;
+
+        // for(int n = 1 ; n <= num ; n++ ){
+        //     System.out.println(n);
+        //     sum  = sum + n;
+        // }
+        // System.out.println(sum);
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Num : ");
+        // int num = sc.nextInt();
+
+        // for(int n = 1 ; n <= 10 ; n++){
+        //     System.out.println(n * num);
+        // }
+
+        System.out.println("MAS");
+
+    }
+}
